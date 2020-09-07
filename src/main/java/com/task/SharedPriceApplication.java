08aa9d13dd889ac068fd0ba9831322c2a9cd798d
Kanza <skanzajamal@gmail.com>
2020-09-07 20:16:05 +0200

    add user detail

--- src/main/java/com/task/SharedPriceApplication.java
+++ src/main/java/com/task/SharedPriceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.Collections;
 
 
 @SpringBootApplication
@@ -25,6 +27,19 @@ public class SharedPriceApplication {
          return new Docket(DocumentationType.SWAGGER_2)
                  .select()
                  .paths(PathSelectors.ant("/stockPrices/*"))
-                 .build();
+                 .build()
+                 .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails(){
+         return new ApiInfo(
+                 "Shared Price Api",
+                 "Application in JAVA",
+                 "1.0",
+                 "Free to use",
+                 new springfox.documentation.service.Contact("Kanza Jamal", "https://github.com/skanzajamal", "k122060@nu.ed.pk"),
+                 "API License",
+                 "https://github.com/skanzajamal",
+                 Collections.emptyList());
     }
 }
